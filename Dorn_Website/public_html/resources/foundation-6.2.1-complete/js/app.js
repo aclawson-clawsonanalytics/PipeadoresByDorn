6b8e7032ca741834a8c72b2c0c90df5b31718b07
Andrew Clawson <andrewjclawson18@gmail.com>
2016-06-22 10:54:03 -0600

    Good design. deploy to server.

--- Dorn_Website/public_html/resources/foundation-6.2.1-complete/js/app.js
+++ Dorn_Website/public_html/resources/foundation-6.2.1-complete/js/app.js
@@ -1,8 +1,4 @@
 $(document).foundation();
-$("#menu").toggleClass("is-open");
-/*
-$(".toggle-menu").on("click", function(){
-            $("#menu").toggleClass("is-open");
-       });
-       */
+
+
 
